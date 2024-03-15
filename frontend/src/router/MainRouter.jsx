f1ceb52c4c876c298c0fb9f31bdd201f4d71c91c
priyankara senarathne <priyankara.sliit@gmail.com>
2024-03-15 09:37:25 +0100

    folder stucture and images setup

---
+++ frontend/src/router/MainRouter.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import MainLayouts from "../layouts/MainLayouts";
+
+const MainRouter = () => {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" Component={MainLayouts}>
+            <Route index Component={Home} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
+
+export default MainRouter;
