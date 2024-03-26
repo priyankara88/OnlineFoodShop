e44bcea4eea7411309d6c08d3fe3d50afc8938e6
priyankara senarathne <priyankara.sliit@gmail.com>
2024-03-26 22:07:17 +0100

    card

--- frontend/src/components/foodDisplay/FoodItemCard.jsx
+++ frontend/src/components/foodDisplay/FoodItemCard.jsx
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 const FoodItemCard = ({ id, name, image, price, description, category }) => {
   const [buttonFlag, setButtonFlg] = useState(0);
+  const [Qty, setQty] = useState(1);
 
-  const HandlFlagEvent = () => {
-    console.log("button click");
+  const HandlFlagEvent = (id) => {
+    setButtonFlg(id);
+  };
+  // Handele Increment of qty button click  event
+
+  const HandeleDecrement = () => {
+    setQty((pre) => pre - 1);
+  };
+  // Handle Increment of qty button click event
+
+  const HandleIncrement = () => {
+    setQty((pre) => pre + 1);
   };
 
+  console.log(Qty);
   return (
     <div className="relative w-[200px] mt-6 rounded-b-lg ">
       <div
@@ -19,10 +31,33 @@ const FoodItemCard = ({ id, name, image, price, description, category }) => {
         }}
         className="rounded-t-lg shadow-lg "
       />
-      <div className="absolute md:right-3 md:top-36 cursor-pointer ">
+      <div key={id} className="absolute md:right-3 md:top-36 cursor-pointer ">
         <div
-          onClick={HandlFlagEvent}
+          onClick={() => HandlFlagEvent(id)}
           className="w-7 h-7 rounded-full border-2 border-zinc-600 bg-white flex items-center justify-center"
+        >
+          +
+        </div>
+      </div>
+
+      <div
+        className={`${
+          buttonFlag === id
+            ? "absolute right-3 md:top-36 flex items-center justify-center w-17 h-8 border-2 rounded-full bg-white"
+            : "hidden absolute right-3 md:top-36  items-center justify-center w-17 h-8 border-2 rounded-full bg-white"
+        } `}
+      >
+        <div
+          onClick={() => HandeleDecrement()}
+          className="w-7 h-7 flex items-center justify-center rounded-full border-2 border-zinc-600 bg-green-300 cursor-pointer"
+        >
+          -
+        </div>
+        <span>{Qty}</span>
+
+        <div
+          onClick={() => HandleIncrement()}
+          className="w-7 h-7 flex items-center justify-center rounded-full border-2 border-zinc-600 bg-pink-300 cursor-pointer"
         >
           +
         </div>
